c7246659c6e72ac0af99e853e25ee4851f590919
Jay Sanders <mindtonic@gmail.com>
2012-03-28 11:30:10 -0400

    jQuery Countdown Plugin

---
+++ countdown.js
@@ -0,0 +1,55 @@
+(function( $ ) {
+  $.fn.countdown = function(options) {
+
+    // Default Settings
+    var settings = $.extend( {
+      'limit'       : 85,
+      'counter_div' : '_countdown_counter_'
+    }, options);
+
+    this.after($('<div>').attr({ class: settings['counter_div'] }));
+
+    // Plugin
+    return this.each(function() {
+
+      var limit = settings['limit'];
+      var counter = $(this).next();
+
+      counter.addClass('_countdown_counter_');
+      counter.html('Please limit your comment to '+limit+' characters.');
+
+      $(this).keyup(function(){
+        var new_length = $(this).val().length;
+        if (new_length > limit)
+        {
+          text = $(this);
+          text.val(text.val().substring(0, limit))
+          return false;
+        }
+        else
+        {
+          counter.html('You have '+ (limit - new_length) +' characters left.');
+          return true;
+        }
+      });
+
+      $(this).blur(function(){
+        counter.html('You have used '+ $(this).val().length +' of ' + limit + ' characters.');
+      });
+    });
+  };
+})( jQuery );
+
+
+
+// Diagnostics Script to Debug in Console
+$.d = function (item) {
+  if (console.info && console.log) {
+    if (typeof item == "object") {
+      console.log(item)
+    } else {
+      var date = new Date();
+      console.info(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " [" + item + "] ")
+    }
+  }
+};
